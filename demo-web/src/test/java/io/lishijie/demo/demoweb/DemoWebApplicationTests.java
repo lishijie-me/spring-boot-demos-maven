ab8d5ac209b13789187c5d43f6acc09c441706b2
lishijie-me <2424782003@qq.com>
2024-05-27 19:07:35 +0800

    demo-web案例代码待办

---
+++ demo-web/src/test/java/io/lishijie/demo/demoweb/DemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lishijie.demo.demoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
