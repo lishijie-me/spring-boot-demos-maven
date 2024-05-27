8074d37c73b9d7a9ae516f909ca97e304f393ca2
lishijie-me <2424782003@qq.com>
2024-05-27 18:41:18 +0800

    demo-banner案例展示

---
+++ demo-banner/src/test/java/io/lishijie/demo/demobanner/DemoBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lishijie.demo.demobanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
